b499b60a20734682a8bc06ae704e036729c91643
Robert Petreski <robert.petreski@digital360.com.au>
2014-11-26 15:58:55 +1100

    Add a link to checkout if the order was not fulfilled previously

--- code/order/Order.php
+++ code/order/Order.php
@@ -435,6 +435,9 @@ class Order extends DataObject implements PermissionProvider {
 	
 	/**
 	 * Generate the URL for viewing this order on the frontend
+	 *
+	 * Will default to the account repay page unless the order
+	 * is still in the cart
 	 * 
 	 * @see PaypalExpressCheckoutaPayment_Handler::doRedirect()
 	 * @return String URL for viewing this order
@@ -442,6 +445,9 @@ class Order extends DataObject implements PermissionProvider {
 	public function Link() {
 		//get the account page and go to it
 		$account = DataObject::get_one('AccountPage');
+		if (strtolower($this->Status) == 'cart') {
+			return DataObject::get_one('CheckoutPage')->Link() . 'order/' . $this->ID;
+		}
 		return $account->Link()."order/$this->ID";
 	}
 
